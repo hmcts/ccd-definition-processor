f5234e4be68c12d1558ab0d94152992f785756a3
Damian Dunajski <damian.dunajski@hmcts.net>
2018-10-18 17:44:45 +0100

    Add .editorconfig and reformat ESLint config file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,30 @@
 module.exports = {
-    "env": {
-        "es6": true,
-        "node": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": 2018
-    },
-    "rules": {
-        "indent": [
-            "error",
-            2
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "no-console": [
-            "error",
-            {
-                allow: ["log", "warn", "error"]
-            }
-        ]
-    }
+  "env": {
+    "es6": true,
+    "node": true
+  },
+  "extends": "eslint:recommended",
+  "parserOptions": {
+    "ecmaVersion": 2018
+  },
+  "rules": {
+    "indent": [
+      "error",
+      2
+    ],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "quotes": [
+      "error",
+      "single"
+    ],
+    "no-console": [
+      "error",
+      {
+        allow: ["log", "warn", "error"]
+      }
+    ]
+  }
 };

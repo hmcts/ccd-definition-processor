d74c298214edb9f13dd5a9c0c3fd33d43c9c03de
Damian Dunajski <damian.dunajski@hmcts.net>
2018-10-19 15:47:23 +0100

    Add simple json2xlsx test

--- src/test/json2xlsx.js
+++ src/test/json2xlsx.js
@@ -1,6 +1,10 @@
 const assert = require('assert');
+const XLSX = require('xlsx');
 
 const run = require('../main/json2xlsx');
+
+const fileUtils = require('../main/lib/file-utils');
+const asyncUtils = require('../main/lib/async-utils');
 
 describe('json2xlsx', () => {
   describe('validation', () => {
@@ -45,5 +49,24 @@ describe('json2xlsx', () => {
         assert.equal(err, 'AssertionError [ERR_ASSERTION]: sheets directory argument (-D) is required');
       }
     });
+  });
+
+  describe('outcome', () => {
+    it('should create empty JSON files from embedded template', async () => {
+      await run({
+        _: [],
+        sourceXlsx: './data/ccd-template.xlsx',
+        destinationXlsx: './temp/ccd-definitions.xlsx',
+        sheetsDir: './temp'
+      });
+
+      const sheets = Object.keys(XLSX.readFile('./temp/ccd-definitions.xlsx').Sheets)
+      assert(sheets.length > 0, 'No sheets have been created');
+
+      const files = fileUtils.listJsonFilesInFolder('./temp');
+      asyncUtils.forEach(files, file => {
+        assert(sheets.includes(file.slice(0, -5)), `Sheet corresponding to JSON file ${file} does not exist`);
+      });
+    }).timeout(15000);
   });
 });

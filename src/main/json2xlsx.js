612f3b2f8e7c7c4edbe645f5a279d4c9552e4451
Damian Dunajski <damian.dunajski@hmcts.net>
2018-10-23 14:44:39 +0100

    Remove ability to provide source XLSX when json2xls command is run + remove ability to clean XLSX

--- src/main/json2xlsx.js
+++ src/main/json2xlsx.js
@@ -5,48 +5,31 @@ const fileUtils = require('./lib/file-utils');
 const asyncUtils = require('./lib/async-utils');
 const ccdUtils = require('./lib/ccd-spreadsheet-utils');
 
-const validateArgs = (args) => {
-  if (args.clear) {
-    assert(!!args.sourceXlsx, 'source spreadsheet file argument (-i) is required');
-    assert(fileUtils.exists(args.sourceXlsx), `spreadsheet file ${args.sourceXlsx} not found`);
-  } else {
-    assert(!!args.sourceXlsx, 'source spreadsheet file argument (-i) is required');
-    assert(!!args.destinationXlsx, 'destination spreadsheet file argument (-o) is required');
-    assert(!!args.sheetsDir, 'sheets directory argument (-D) is required');
+const sourceXlsx = './data/ccd-template.xlsx';
 
-    assert(fileUtils.exists(args.sourceXlsx), `source spreadsheet file ${args.sourceXlsx} not found`);
-    assert(fileUtils.exists(args.sheetsDir), `sheets directory ${args.sheetsDir} not found`);
-  }
+const validateArgs = (args) => {
+  assert(!!args.sheetsDir, 'sheets directory argument (-D) is required');
+  assert(!!args.destinationXlsx, 'spreadsheet file argument (-o) is required');
+
+  assert(fileUtils.exists(args.sheetsDir), `sheets directory ${args.sheetsDir} not found`);
 };
 
 const run = async (args) => {
   validateArgs(args);
 
-  console.log(`Import...\n loading workbook: ${args.sourceXlsx}`);
-  const builder = new ccdUtils.SpreadsheetBuilder(args.sourceXlsx);
+  console.log(`Import...\n loading workbook: ${sourceXlsx}`);
+  const builder = new ccdUtils.SpreadsheetBuilder(sourceXlsx);
   await builder.loadAsync();
 
-  let sheets;
-  if (args.clear) {
-    sheets = args._.length > 0 ? args._ : builder.allSheets().map(sheet => sheet.name());
+  const sheets = args._.length > 0 ? args._ : fileUtils
+    .listJsonFilesInFolder(args.sheetsDir)
+    .map((filename) => filename.slice(0, -5));
 
-    await asyncUtils.forEach(sheets, async (sheet) => {
-      console.log(`  clearing sheet: ${sheet}`);
-      builder.clearSheetData(sheet);
-    });
-
-    args.destinationXlsx = args.sourceXlsx;
-  } else {
-    sheets = args._.length > 0 ? args._ : fileUtils
-      .listJsonFilesInFolder(args.sheetsDir)
-      .map((filename) => filename.slice(0, -5));
-
-    await asyncUtils.forEach(sheets, async (sheet) => {
-      const jsonPath = path.join(args.sheetsDir, `${sheet}.json`);
-      console.log(`  importing sheet data: ${jsonPath}`);
-      builder.updateSheetDataJson(sheet, await fileUtils.readJson(jsonPath));
-    });
-  }
+  await asyncUtils.forEach(sheets, async (sheet) => {
+    const jsonPath = path.join(args.sheetsDir, `${sheet}.json`);
+    console.log(`  importing sheet data: ${jsonPath}`);
+    builder.updateSheetDataJson(sheet, await fileUtils.readJson(jsonPath));
+  });
 
   console.log(` saving workbook: ${args.destinationXlsx}`);
   await builder.saveAsAsync(args.destinationXlsx);
--- src/test/json2xlsx.js
+++ src/test/json2xlsx.js
@@ -8,45 +8,29 @@ const asyncUtils = require('../main/lib/async-utils');
 
 describe('json2xlsx', () => {
   describe('validation', () => {
-    it('should throw an error when source spreadsheet file argument is not provided', async () => {
-      try {
-        await run({
-          _: [],
-          sourceXlsx: '',
-          destinationXlsx: './temp/ccd-definitions.xlsx',
-          sheetsDir: './temp'
-        });
-        assert.fail('No error has been thrown');
-      } catch (err) {
-        assert.equal(err, 'AssertionError [ERR_ASSERTION]: source spreadsheet file argument (-i) is required');
-      }
-    });
-
-    it('should throw an error when destination spreadsheet file argument is not provided', async () => {
-      try {
-        await run({
-          _: [],
-          sourceXlsx: './data/ccd-template.xlsx',
-          destinationXlsx: '',
-          sheetsDir: './temp'
-        });
-        assert.fail('No error has been thrown');
-      } catch (err) {
-        assert.equal(err, 'AssertionError [ERR_ASSERTION]: destination spreadsheet file argument (-o) is required');
-      }
-    });
-
     it('should throw an error when sheets directory argument is not provided', async () => {
       try {
         await run({
           _: [],
-          sourceXlsx: './data/ccd-template.xlsx',
-          destinationXlsx: './temp/ccd-definitions.xlsx',
-          sheetsDir: ''
+          sheetsDir: '',
+          destinationXlsx: './temp/ccd-definitions.xlsx'
         });
         assert.fail('No error has been thrown');
       } catch (err) {
         assert.equal(err, 'AssertionError [ERR_ASSERTION]: sheets directory argument (-D) is required');
+      }
+    });
+
+    it('should throw an error when spreadsheet file argument is not provided', async () => {
+      try {
+        await run({
+          _: [],
+          sheetsDir: './temp',
+          destinationXlsx: ''
+        });
+        assert.fail('No error has been thrown');
+      } catch (err) {
+        assert.equal(err, 'AssertionError [ERR_ASSERTION]: spreadsheet file argument (-o) is required');
       }
     });
   });
@@ -55,9 +39,8 @@ describe('json2xlsx', () => {
     it('should create XLSX file from JSON fixtures', async () => {
       await run({
         _: [],
-        sourceXlsx: './data/ccd-template.xlsx',
-        destinationXlsx: './temp/ccd-definitions.xlsx',
-        sheetsDir: './src/test/fixtures'
+        sheetsDir: './src/test/fixtures',
+        destinationXlsx: './temp/ccd-definitions.xlsx'
       });
 
       const sheets = XLSX.readFile('./temp/ccd-definitions.xlsx').Sheets;
@@ -69,6 +52,6 @@ describe('json2xlsx', () => {
         assert(sheets[sheetName], `No sheet corresponding to JSON file ${file} exists`);
         assert.equal(sheets[sheetName]['A4'].v, 42736, `Unexpected value found in A4 cell of ${sheetName} sheet`);
       });
-    }).timeout(15000);
+    });
   });
 });

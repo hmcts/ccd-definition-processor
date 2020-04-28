f61213a3877a8cdf96597880148733f1663c4120
J. Victor Ferreira <victor.ferreira@hmcts.net>
2020-04-28 17:14:06 +0100

    Addressing lint issues

--- src/main/lib/file-utils.js
+++ src/main/lib/file-utils.js
@@ -37,22 +37,21 @@ const getJsonFilePaths = (directory, exclusions = []) => {
     path => !exclusions.some(exclusion => path.split('/').some(chunk => matcher.isMatch(chunk, exclusion))));
 };
 
-function toRelativePaths(array, root) {
+function toRelativePaths (array, root) {
   return array.map(file => path.relative(root, file))
-    .sort(function (a, b) {
-      const filename_a = a.replace('\.json', '');
-      const filename_b = b.replace('\.json', '');
+    .sort((a, b) => {
+      const firstFilename = a.replace('.json', '');
+      const secondFilename = b.replace('.json', '');
 
       //Looking for files with similar names
-      if (filename_b.startsWith(filename_a)) {
+      if (secondFilename.startsWith(firstFilename)) {
         //Found files that have nearly identical names. Put longer name later.
         return -1;
       } else {
         //Otherwise, just return in their original order
         return 0;
       }
-    })
-    ;
+    });
 }
 
 module.exports = {

1e0647eedb4b781851f5a3e735e48305746974c4
REMYA MARY JAMES <RemyaMary.James@hmcts.net>
2022-12-06 17:45:39 +0000

    updated

--- src/test/json2xlsx.js
+++ src/test/json2xlsx.js
@@ -15,6 +15,7 @@ const sheetNames = [
   'CaseRoles',
   'CaseType',
   'CaseTypeTab',
+  'Categories',
   'ComplexTypes',
   'ChallengeQuestion',
   'FixedLists',
@@ -189,6 +190,14 @@ describe('json2xlsx', () => {
           assertCell(sheetName, 'F4', 'The Question');
           assertCell(sheetName, 'I3', 'Answer');
           assertCell(sheetName, 'I4', 'My Answer');
+        }
+        if (sheetName === 'Categories') {
+          assertCell(sheetName, 'D3', 'CategoryID');
+          assertCell(sheetName, 'D4', 'Category');
+          assertCell(sheetName, 'E3', 'CategoryLabel');
+          assertCell(sheetName, 'E4', 'CategoryName');
+          assertCell(sheetName, 'F3', 'DisplayOrder');
+          assertCell(sheetName, 'F4', '1');
         }
       });
 
